2666f3d4b40deb2017554674c6a01829e6e4de7f
Asamu <loki10214@gmail.com>
2014-12-29 03:39:48 +0800

    data:tagToken() finish

--- TagTool/src/Data.java
+++ TagTool/src/Data.java
@@ -6,7 +6,7 @@ public class Data {
 	public String tags;			//一個字串,以","之類的字元分割出各個tag (用strtok之類的?)
 	
 	private String tagList[];	//存分割後的tag
-	private int tagNum=0;		//有幾個tag
+	private int tagCount=0;		//有幾個tag
 	
 	public boolean dataAvailable(){	//檔案是否存在檔案系統
 		
@@ -24,18 +24,20 @@ public class Data {
 		else		
 			return true;
 	}
-	public void removeData(){	// 就把此檔案的tag都拿掉,data的數量-1就好
+	public void removeData(){	// 就把此檔案的tag都拿掉,主程式端data的數量-1就好
+		tags="";
+		tagCount=0;
 		
 	}
 	
 	private void tagToken(String tagString){
 		StringTokenizer Tok=new StringTokenizer(tagString,",");	//以","做區隔
 		String buffer="";
-		tagNum=0;		//等於每次做都刷新tagList陣列
+		
 		while (Tok.hasMoreElements()){
 			buffer=(String) Tok.nextElement();
-			if(searchTag(buffer)!=0){
-				tagList[++tagNum]=buffer;	//注意陣列索引值是從1~tagNum,0不使用
+			if(searchTag(buffer)==0){
+				tagList[++tagCount]=buffer;	//注意陣列索引值是從1~tagCount,0暫不使用
 			}
 		}
 		
@@ -43,7 +45,7 @@ public class Data {
 	
 	public int searchTag(String tag){
 		int index=0;					//若return 0 代表該tag不存在
-		for(int i=1;i<=tagNum;i++){
+		for(int i=1;i<=tagCount;i++){
 			if(tag==tagList[i]){
 				index=i;
 				return index;
