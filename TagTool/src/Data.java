dd10a28e1fe0d4f771232b9e8631265a3f7b404b
Asamu <loki10214@gmail.com>
2014-12-28 16:35:13 +0800

    initial tag and data

--- TagTool/src/Data.java
+++ TagTool/src/Data.java
@@ -1,8 +1,29 @@
 
 public class Data {
-	int test=1;
-
-	void main(){
-		test=3;
+	public String directory;	//包含路徑跟檔名
+	public String tags;			//一個字串,以","之類的字元分割出各個tag (用strtok之類的?)
+	
+	
+	public boolean dataAvailable(){	//檔案是否存在檔案系統
+		
+		return true;
 	}
+	
+	public void changeTag(String inputString){	//供UI調用,change tag按鈕按下去時候
+		tags=inputString;
+	}
+	
+	public boolean tagExist(){	//該data是否有tag,已做完
+		if(tags=="")
+			return false;
+		else		
+			return true;
+	}
+	public void removeData(){	// 就把此檔案的tag都拿掉,data的數量-1就好
+		
+	}
+	
+	
+
+
 }
--- TagTool/src/Tags.java
+++ TagTool/src/Tags.java
@@ -1,4 +1,21 @@
 
 public class Tags {
-
+	private String tagName;
+	private int userLevel;
+	private String[] dataList;		//紀錄哪些data用到此tag(紀錄name)
+	private int dataCount=0;		//有幾個data
+	
+	
+	
+	
+	
+	
+	public String getTagName(){		//取得tag的name
+		return tagName;
+	}
+	public void changeTagName(String newName){	//修改tag的name
+		tagName=newName;
+	}
+	
+	
 }
